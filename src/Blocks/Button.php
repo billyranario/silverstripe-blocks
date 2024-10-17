f2809ae397735e96e1902e1ad5d8830f0550fdc9
billy joel ranario <billy.ranario@ustp.edu.ph>
2024-10-17 21:13:44 +0800

    test

--- src/Blocks/Button.php
+++ src/Blocks/Button.php
@@ -17,26 +17,39 @@ class Button extends BaseElement
         'ButtonLabel' => 'Varchar(255)',
         'ButtonClass' => 'Varchar(255)',
         'RedirectLink' => 'Varchar(255)',
-        'Width' => 'Enum("auto, full", "auto")',
+        'Width' => 'Enum("w-auto, w-full")',
         'OpenNewTab' => 'Boolean'
+    ];
+
+    private static $defaults = [
+        'ButtonClass' => 'bg-primary hover:bg-primary-dark px-8 py-3',
+        'Width' => 'w-auto'
     ];
 
     public function getType(): string
     {
-        return 'Button Block';
+        return 'Button';
+    }
+
+    public function populateDefaults()
+    {
+        parent::populateDefaults();
+
+        $this->ButtonClass = 'bg-primary hover:bg-primary-dark px-8 py-3';
+        $this->Width = 'w-auto';
     }
 
     public function getCMSFields()
     {
         $fields = parent::getCMSFields();
-
         $fields->removeByName('Title');
 
         $fields->addFieldToTab('Root.Main', TextField::create('ButtonLabel', 'Button Label'));
         $fields->addFieldToTab(
             'Root.Main',
             TextField::create('ButtonClass', 'Button CSS Class')
-                ->setValue('bg-primary hover:bg-primary-dark px-8 py-3')
+                ->setDescription('Default: bg-primary hover:bg-primary-dark px-8 py-3')
+                ->setValue($this->ButtonClass)
         );
         $fields->addFieldToTab('Root.Main', TextField::create('RedirectLink', 'Redirect Link'));
         $fields->addFieldToTab(
@@ -46,9 +59,20 @@ class Button extends BaseElement
                     'w-auto' => 'Auto',
                     'w-full' => 'Full Width'
                 ])
+                ->setEmptyString('(None)')
+                ->setValue($this->Width)
         );
         $fields->addFieldToTab('Root.Main', CheckboxField::create('OpenNewTab', 'Open in a new tab'));
 
         return $fields;
+    }
+
+    public function onBeforeWrite()
+    {
+        parent::onBeforeWrite();
+
+        if (!$this->Title || $this->isChanged('ButtonLabel')) {
+            $this->Title = "Button: ". $this->ButtonLabel;
+        }
     }
 }
